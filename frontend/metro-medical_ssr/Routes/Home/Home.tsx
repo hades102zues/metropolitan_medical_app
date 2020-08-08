6fa68fdbda161c2939f628d9f70e6290647a9b18
Joshua Wiggins <jwiggins.works@gmail.com>
2020-08-08 00:10:21 -0400

    Redesigns the about section

--- frontend/metro-medical_ssr/Routes/Home/Home.tsx
+++ frontend/metro-medical_ssr/Routes/Home/Home.tsx
@@ -92,7 +92,7 @@ const Home = () => {
         </section>
       </div>
 
-      <section className={styles.about}>
+      {/* <section className={styles.about}>
         <div className="wrapper">
           <h2 className={styles.aboutHeadline}>Who we are</h2>
           <div className={styles.aboutStory}>
@@ -109,6 +109,36 @@ const Home = () => {
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
               reprehenderit in voluptate
+            </p>
+          </div>
+        </div>
+      </section> */}
+      <section className={styles.about}>
+        <div className={styles.about1}>
+          <div className={styles.aboutImage}>
+            <img src="https://images.unsplash.com/photo-1579840900569-f266a155415f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" />
+          </div>
+        </div>
+        <div className={styles.about2}>
+          <div className={styles.adjust}>
+            <h2 className={styles.aboutTitle}>
+              Who <br />
+              We Are
+            </h2>
+          </div>
+          <div className={styles.aboutTalk}>
+            <p className={styles.talk}>
+              Metropolitan Medical was founded and is run by Dr. Camille Bowen
+              (BSc, BSc, MBBS, MPH) a Barbadian with a passion for excellent
+              patient care with a particular emphasis on women's health and
+              wellness.
+            </p>
+            <p className={styles.talk}>
+              At our clinic we offer an array of medical services including
+              family planning and pap smears. We also have a passion for public
+              health and offer free or low cost screening at intervals as well
+              as disseminate health information via social media, email and hand
+              outs.
             </p>
           </div>
         </div>
