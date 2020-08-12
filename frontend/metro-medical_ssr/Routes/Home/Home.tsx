e527a33df87a1c309e5c5648b2571e0d38b7c700
Joshua Wiggins <jwiggins.works@gmail.com>
2020-08-12 14:37:14 -0400

    Cssed the About

--- frontend/metro-medical_ssr/Routes/Home/Home.tsx
+++ frontend/metro-medical_ssr/Routes/Home/Home.tsx
@@ -5,12 +5,12 @@ import Header from "../shared/UI/Header/Header";
 import Footer from "../shared/UI/Footer/Footer";
 import Drawer from "../shared/UI/Drawer/Drawer";
 import Hero from "./Sections/Hero/Hero";
+import About from "../shared/About/About";
 
 import Link from "next/link";
 
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
-import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
 
 import { withStyles } from "@material-ui/core";
 
@@ -100,6 +100,7 @@ const Home = () => {
         <Header drawerHandler={drawerOpener} />
       </Hero>
 
+      <About />
       {/* <section className={styles.about}>
         <div className="wrapper">
           <h2 className={styles.aboutHeadline}>Who we are</h2>
@@ -121,50 +122,6 @@ const Home = () => {
           </div>
         </div>
       </section> */}
-      <section className={styles.about}>
-        <div className={styles.aboutImage}>
-          <img src="https://images.unsplash.com/photo-1579840900569-f266a155415f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" />
-        </div>
-
-        <div className="wrapper">
-          <div className={styles.aboutFlex}>
-            <div className={styles.about2}>
-              <div className={styles.adjust}>
-                <h2 className={styles.aboutTitle}>
-                  Who <br />
-                  We Are
-                </h2>
-              </div>
-              <div className={styles.aboutTalk}>
-                <p className={styles.talk}>
-                  Metropolitan Medical was founded and is run by Dr. Camille
-                  Bowen (BSc, BSc, MBBS, MPH) a Barbadian with a passion for
-                  excellent patient care with a particular emphasis on women's
-                  health and wellness.
-                </p>
-                <p className={styles.talk}>
-                  At our clinic we offer an array of medical services including
-                  family planning and pap smears. We also have a passion for
-                  public health and offer free or low cost screening at
-                  intervals as well as disseminate health information via social
-                  media, email and hand outs.
-                </p>
-              </div>
-            </div>
-            <div className={styles.about3}>
-              <div className={styles.mission}>
-                <h3 className={styles.mission_title}>Mission</h3>
-                <LocalHospitalIcon />
-                <p className={styles.mission_statement}>
-                  Our mission here at Metropolitan Medical is to bring you high
-                  quality healthcare at an affordable cost in a central location
-                  with a beautiful environment.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
 
       <section className={styles.services}>
         <div className="wrapper">
---
+++ frontend/metro-medical_ssr/Routes/shared/About/About.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "./About.module.css";
+
+import { withStyles } from "@material-ui/core";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import PeopleIcon from "@material-ui/icons/People";
+
+const About = () => {
+  const StylMissionIcon = withStyles({
+    root: {
+      fontSize: 35,
+      color: "#dd6a8f",
+      marginRight: 10,
+    },
+  })(LocalHospitalIcon);
+
+  const StylePeopleIcon = withStyles({
+    root: {
+      fontSize: 40,
+      color: "#583b4c",
+      marginRight: 10,
+    },
+  })(PeopleIcon);
+
+  return (
+    <section className={styles.about}>
+      <div className={styles.aboutImage}>
+        <img src="https://images.unsplash.com/photo-1579840900569-f266a155415f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" />
+      </div>
+
+      <div className="wrapper">
+        <div className={styles.aboutFlex}>
+          <div className={styles.about2}>
+            <div className={styles.adjust}>
+              <StylePeopleIcon />
+              <h3 className={styles.aboutTitle}>Who We Are</h3>
+            </div>
+            <div className={styles.aboutTalk}>
+              <p className={styles.talk}>
+                Metropolitan Medical was founded and is run by Dr. Camille Bowen
+                (BSc, BSc, MBBS, MPH) a Barbadian with a passion for excellent
+                patient care with a particular emphasis on women's health and
+                wellness.
+              </p>
+              <p className={styles.talk}>
+                At our clinic we offer an array of medical services including
+                family planning and pap smears. We also have a passion for
+                public health and offer free or low cost screening at intervals
+                as well as disseminate health information via social media,
+                email and hand outs.
+              </p>
+            </div>
+          </div>
+          <div className={styles.about3}>
+            <div className={styles.mission}>
+              <StylMissionIcon />
+              <h3 className={styles.mission_title}>Mission</h3>
+
+              <p className={styles.mission_statement}>
+                Our mission here at Metropolitan Medical is to bring you high
+                quality healthcare at an affordable cost in a central location
+                with a beautiful environment.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
