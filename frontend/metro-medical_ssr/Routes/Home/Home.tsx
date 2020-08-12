2216e0b7e8e698907633e1345cb940550c9cc0b6
Joshua Wiggins <jwiggins.works@gmail.com>
2020-08-12 14:04:50 -0400

    About layout completed. Awaits css-ing

--- frontend/metro-medical_ssr/Routes/Home/Home.tsx
+++ frontend/metro-medical_ssr/Routes/Home/Home.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
 
 import { withStyles } from "@material-ui/core";
 
@@ -121,32 +122,46 @@ const Home = () => {
         </div>
       </section> */}
       <section className={styles.about}>
-        <div className={styles.about1}>
-          <div className={styles.aboutImage}>
-            <img src="https://images.unsplash.com/photo-1579840900569-f266a155415f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" />
-          </div>
+        <div className={styles.aboutImage}>
+          <img src="https://images.unsplash.com/photo-1579840900569-f266a155415f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80" />
         </div>
-        <div className={styles.about2}>
-          <div className={styles.adjust}>
-            <h2 className={styles.aboutTitle}>
-              Who <br />
-              We Are
-            </h2>
-          </div>
-          <div className={styles.aboutTalk}>
-            <p className={styles.talk}>
-              Metropolitan Medical was founded and is run by Dr. Camille Bowen
-              (BSc, BSc, MBBS, MPH) a Barbadian with a passion for excellent
-              patient care with a particular emphasis on women's health and
-              wellness.
-            </p>
-            <p className={styles.talk}>
-              At our clinic we offer an array of medical services including
-              family planning and pap smears. We also have a passion for public
-              health and offer free or low cost screening at intervals as well
-              as disseminate health information via social media, email and hand
-              outs.
-            </p>
+
+        <div className="wrapper">
+          <div className={styles.aboutFlex}>
+            <div className={styles.about2}>
+              <div className={styles.adjust}>
+                <h2 className={styles.aboutTitle}>
+                  Who <br />
+                  We Are
+                </h2>
+              </div>
+              <div className={styles.aboutTalk}>
+                <p className={styles.talk}>
+                  Metropolitan Medical was founded and is run by Dr. Camille
+                  Bowen (BSc, BSc, MBBS, MPH) a Barbadian with a passion for
+                  excellent patient care with a particular emphasis on women's
+                  health and wellness.
+                </p>
+                <p className={styles.talk}>
+                  At our clinic we offer an array of medical services including
+                  family planning and pap smears. We also have a passion for
+                  public health and offer free or low cost screening at
+                  intervals as well as disseminate health information via social
+                  media, email and hand outs.
+                </p>
+              </div>
+            </div>
+            <div className={styles.about3}>
+              <div className={styles.mission}>
+                <h3 className={styles.mission_title}>Mission</h3>
+                <LocalHospitalIcon />
+                <p className={styles.mission_statement}>
+                  Our mission here at Metropolitan Medical is to bring you high
+                  quality healthcare at an affordable cost in a central location
+                  with a beautiful environment.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
