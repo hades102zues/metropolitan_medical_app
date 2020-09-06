affc1c434df9a04e8f7dbcd9fcb16818796a3b40
Joshua Wiggins <jwiggins.works@gmail.com>
2020-09-06 18:27:48 -0400

    Back to the drawing board

--- frontend/metro-medical_ssr/Routes/Post/Post.tsx
+++ frontend/metro-medical_ssr/Routes/Post/Post.tsx
@@ -3,9 +3,11 @@ import styles from "./Post.module.css";
 import PageFrame from "../shared/UI/PageFrame/PageFrame";
 
 const { gutenbergToReact } = require("@threemammals/gutenberg-to-react");
+import WPGBlocks from "react-gutenberg";
+import parse from "html-react-parser";
 
 interface Post {
-  content: string;
+  content: any;
   date: string;
   title: string;
   excerpt: string;
@@ -18,31 +20,50 @@ const Post: React.FC<PostInput> = (props) => {
   const { post } = props;
   let render: JSX.Element[] | JSX.Element;
 
-  let exp = null;
-  console.log(post);
-  if (!post.postExist) {
-    render = <h3 className={styles.post_heading}>Post not found.</h3>;
-  } else {
-    let items = gutenbergToReact(post.content);
-    items.forEach((item, i) => {
-      console.log(item.props.children[0]);
-    });
+  let exp: JSX.Element | JSX.Element[] = null;
 
-    exp = items.map((item) => (
-      <React.Fragment>
-        {" "}
-        {item.props.children[0]}
-        <br />
-      </React.Fragment>
-    ));
-    render = (
-      <React.Fragment>
-        <h3 className={styles.post_heading}>{post.title}</h3>
-        <p className={styles.post_date}>{post.date}</p>
-        {exp}
-      </React.Fragment>
-    );
-  }
+  // if (!post.postExist) {
+  //   render = <h3 className={styles.post_heading}>Post not found.</h3>;
+  // } else {
+  //   exp = parse(post.content);
+  //   console.log(exp);
+  //   render = exp;
+
+  // let items = gutenbergToReact(post.content);
+
+  // exp = items.map((item, i) => {
+  //   console.log(item);
+  // });
+  // if (i === 0)
+  //   console.log(
+  //     item.props.children[0][0].type,
+  //     item.props.children[0][0].props.children[0].props.children[0].props
+  //   );
+  // if (
+  //   item.props.children[0][0].type === "p" ||
+  //   item.props.children[0][0].type === "img" ||
+  //   item.props.children[0][0].type === "div"
+  // ) {
+  //   return (
+  //     <React.Fragment>
+  //       {" "}
+  //       {item.props.children[0]}
+  //       <br />
+  //     </React.Fragment>
+  //   );
+  // } else {
+  //   return <React.Fragment></React.Fragment>;
+  // }
+  //});
+
+  // render = (
+  //   <React.Fragment>
+  //     <h3 className={styles.post_heading}>{post.title}</h3>
+  //     <p className={styles.post_date}>{post.date}</p>
+  //     {exp}
+  //   </React.Fragment>
+  // );
+  //}
 
   return (
     //require a head component
